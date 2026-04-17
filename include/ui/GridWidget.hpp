5cb64e4fa0355f649022426b716640d69de74925
rangeset <Rangeset24@outlook.com>
2026-04-18 02:11:42 +0800

    feat(ui): add background color.

--- include/ui/GridWidget.hpp
+++ include/ui/GridWidget.hpp
@@ -9,8 +9,14 @@
 
 #include <QWidget>
 
+constexpr QColor BACKGROUND_COLOR(135, 150, 186);
+constexpr QColor SNAKE_COLOR(160, 193, 128);
+constexpr QColor APPLE_COLOR(198, 45, 45);
+
 class GridWidget : public QWidget {
     int size_;
 public:
     GridWidget(int size, QWidget* parent = nullptr);
+protected:
+    void paintEvent(QPaintEvent* event) override;
 };
--- src/ui/GridWidget.cpp
+++ src/ui/GridWidget.cpp
@@ -6,9 +6,16 @@
 // Grid layout widget.
 
 #include <ui/GridWidget.hpp>
+#include <QPainter>
 
 GridWidget::GridWidget(const int size, QWidget* parent) : QWidget(parent) {
     size_ = size;
     this->setFixedSize(size_*30, size_*30);
+    this->update();
     this->show();
+}
+
+void GridWidget::paintEvent(QPaintEvent *event) {
+    QPainter p(this);
+    p.fillRect(this->rect(), BACKGROUND_COLOR);
 }
